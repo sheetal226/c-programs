53be69fae251e20336d8c6ed27a319b14954f16b
sheetal sharma <sheetal26122005@gmail.com>
2026-05-29 13:56:26 +0530

    program to calculate square root

---
+++ basics/Square_root.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter num : ");
+    scanf("%d",&num);
+    // calculate square of number
+    int square = num*num;
+    printf("suare of num is : %d\n",square);
+    return 0;
+}
